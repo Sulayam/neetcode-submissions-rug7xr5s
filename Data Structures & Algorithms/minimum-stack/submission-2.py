2a6efa0b1176610c72298eb111c5d46ae19f525e
Sulayam AR <111346995+Sulayam@users.noreply.github.com>
2026-04-16 07:38:09 +0400

    Add: minimum-stack - submission-2

---
+++ Data Structures & Algorithms/minimum-stack/submission-2.py
@@ -0,0 +1,22 @@
+class MinStack:
+    def __init__(self):
+        self.stack = []
+        self.minStack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        if self.minStack:
+            current_min=self.minStack[-1]
+            new_min=min(current_min,val)
+            self.minStack.append(new_min)
+        else:
+            self.minStack.append(val)
+    def pop(self) -> None:
+        self.stack.pop()
+        self.minStack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.minStack[-1]
